1f8d733e80cf19ee976d1e5a7c50373cbbf7a2ce
Zaheer Soebhan <zaheer.soebhan@surf.nl>
2024-04-10 09:22:09 +0200

    OPT: Improving search results for Edusources
    This is one of the results after multiple sessions with users and
    stakeholders where we've tried different boosting configurations

--- search_client/constants.py
+++ search_client/constants.py
@@ -8,12 +8,13 @@ class DocumentTypes(Enum):
 
 SEARCH_FIELDS = {
     DocumentTypes.LEARNING_MATERIAL: [
-        "title^2", "title.analyzed^2", "title.folded^2",
-        "text", "text.analyzed", "text.folded",
-        "description", "description.analyzed", "description.folded",
-        "keywords", "keywords.folded",
-        "authors.name.folded",
-        "publishers", "publishers.folded",
+        "title^5", "title.analyzed", "title.folded^5",
+        "text^3", "text.analyzed", "text.folded^3",
+        "description^3", "description.analyzed", "description.folded^3",
+        "keywords^4", "keywords.folded^4",
+        "authors.name.folded^2",
+        "publishers^2", "publishers.folded^2",
+        "consortium^2", "consortium.folded^2",
         "ideas", "ideas.folded"
     ],
     DocumentTypes.RESEARCH_PRODUCT: [
