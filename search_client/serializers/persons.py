5144bddd331ac3ca98dd0226eacf9b5bdebb15eb
Fako Berkers <email@fakoberkers.nl>
2024-09-02 20:04:40 +0200

    Adds serializer fields for external authors

--- search_client/serializers/persons.py
+++ search_client/serializers/persons.py
@@ -10,7 +10,8 @@ class Contact(BaseModel):
 class Author(BaseModel):
     name: str
     email: EmailStr | None = Field(default=None)
-    external_id: str | None = Field(default=None)
+    external_id: str | None = Field(default=None, description="The id of the author in the source system.")
     dai: str | None = Field(default=None)
     isni: str | None = Field(default=None)
     orcid: str | None = Field(default=None)
+    is_external: bool = Field(default=False, description="This means external author for the Provider of a Product.")
