bb88b372fdb679aaa8757d453287097d5a44f29c
Fako Berkers <email@fakoberkers.nl>
2025-04-02 17:04:20 +0200

    Adds organizations to indices, configuration and serializers

--- search_client/opensearch/configuration/__init__.py
+++ search_client/opensearch/configuration/__init__.py
@@ -2,6 +2,7 @@ from search_client.opensearch.configuration.core import SearchConfiguration
 from search_client.opensearch.configuration.products import (ProductSearchConfiguration,
                                                              build_product_search_configuration)
 from search_client.opensearch.configuration.projects import ProjectSearchConfiguration
+from search_client.opensearch.configuration.organizations import OrganizationSearchConfiguration
 from search_client.opensearch.configuration.legacy import (MultilingualIndicesSearchConfiguration,
                                                            build_multilingual_indices_search_configuration)
 from search_client.opensearch.configuration.presets import (build_presets_search_configuration,
--- search_client/opensearch/indices/__init__.py
+++ search_client/opensearch/indices/__init__.py
@@ -1,2 +1,3 @@
 from search_client.opensearch.indices.products import build_products_index_configuration
 from search_client.opensearch.indices.projects import build_projects_index_configuration
+from search_client.opensearch.indices.organizations import build_organizations_index_configuration
