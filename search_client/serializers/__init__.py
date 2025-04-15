c882644e1df59b5827f8e7374a798681ec35b790
Fako Berkers <email@fakoberkers.nl>
2025-04-15 09:50:02 +0200

    Adds the File serializer to public interface

--- search_client/serializers/__init__.py
+++ search_client/serializers/__init__.py
@@ -2,3 +2,4 @@ from search_client.serializers.products import LearningMaterial, ResearchProduct
 from search_client.serializers.projects import Project
 from search_client.serializers.organizations import Organization
 from search_client.serializers.persons import Researcher
+from search_client.serializers.files import File
