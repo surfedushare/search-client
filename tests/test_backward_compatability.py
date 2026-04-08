a57c9e4d7f2c66c01a8780c0cae01f123f8d3486
Froede Vrolijk <froede.vrolijk@surf.nl>
2026-04-08 15:11:10 +0200

    Update tests

--- tests/test_backward_compatability.py
+++ tests/test_backward_compatability.py
@@ -238,6 +238,7 @@ class TestPydanticToDictConversion(TestCase):
             "parties": [
                 "Wikiwijs Maken"
             ],
+            "published_in": None,
             "research_themes": [
                 "exact_informatica"
             ],
@@ -421,6 +422,7 @@ class TestPydanticToDictConversion(TestCase):
             "parties": [
                 "Wikiwijs Maken"
             ],
+            "published_in": None,
             "research_themes": [
                 "aarde_milieu"
             ],
@@ -959,6 +961,7 @@ class TestPydanticToDictConversion(TestCase):
             "types": [],
             "research_object_type": None,
             "parties": [],
+            "published_in": None,
             "research_themes": [],
             "projects": [],
             "owners": [],
